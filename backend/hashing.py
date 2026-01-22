f5d748e80fad3d5ef7f06e8cdb36e3a66be50ec8
Venkat Prashad A <venkatprashadalwar@gmail.com>
2026-01-23 00:17:57 +0530

    Delete backend directory

--- backend/hashing.py
+++
@@ -1,7 +0,0 @@
-
-def hash(password):
-    import bcrypt
-    # Hash the password using bcrypt
-    password = password.encode('utf-8')
-    hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-    return hashed.decode('utf-8')
